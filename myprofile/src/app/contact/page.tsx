1f84b032ec49acbbd7f239226a26fc00974d184a
Ferdy <ferdydjasni@gmail.com>
2025-02-17 14:56:24 +0700

    fase awal homepage

---
+++ myprofile/src/app/contact/page.tsx
@@ -0,0 +1,22 @@
+export default function Contact() {
+  return (
+    <div className="contact">
+      <div className="leftcon">
+        <div>
+          <img src="man.svg" alt="me" className="image" />
+        </div>
+      </div>
+      <div className="rightcon">
+        <div>
+          <h1>
+            <u>Contact Me</u>
+          </h1>
+          <p>Alamat</p>
+          <p>No Telp</p>
+          <p>email</p>
+          <button>Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ myprofile/src/app/page.tsx
@@ -0,0 +1,35 @@
+import Navbar from "@/components/navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="left">
+        <div>
+        <p>Hello, I'm Ferdy</p>
+        <p>Professional Fullstack Web Development</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+          saepe autem sequi? Rem laborum a, dolores officiis adipisci,
+          distinctio, nesciunt cupiditate voluptatibus sequi ipsa mollitia
+          culpa. Eveniet cumque tempore in. Atque animi quia sunt, rem aliquam
+          tempore pariatur iure rerum repellat eius nihil magni distinctio
+          corporis vero provident nobis quibusdam repudiandae. Nulla dolore quis
+          exercitationem, consectetur nemo consequatur rerum soluta sed labore
+          eveniet sunt, voluptas earum. Perspiciatis, fuga iste nemo quas
+          inventore ad. Blanditiis, sapiente at odit a quisquam ex eius?
+          Voluptas a dicta possimus odit velit id iste culpa, earum cum totam,
+          quod perferendis laboriosam? Ex ab excepturi at a facilis nesciunt hic
+          numquam expedita, illum odit delectus sit quam similique est
+          molestiae. Quisquam minima perferendis odit ducimus aperiam laboriosam
+          suscipit aspernatur voluptatem iure autem labore porro, magni aliquam.
+          Hic, nostrum pariatur atque est repudiandae accusamus, magni dicta
+          ipsa fugiat porro numquam perspiciatis facilis suscipit dolores optio
+          iusto iure.
+        </p>
+        </div>
+      </div>
+      <div className="right"><img src="man.svg" alt="me" className="image"/></div>
+    </div>
+  );
+}
