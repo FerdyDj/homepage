cc7daffad64f2927e234b2dd131c5904685e8e14
Ferdy <ferdydjasni@gmail.com>
2025-02-19 15:40:01 +0700

    2nd commit

--- portofolio/src/components/navbar.tsx
+++ portofolio/src/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex w-screen">
       <div className="h-[60px] w-[50%] bg-blue-500 flex justify-start items-center pl-14 text-white font-bold">
-        <p className="animate-pulse">PORTOFOLIO</p>
+        <p className="animate-pulse">PORTFOLIO</p>
       </div>
       <div className="h-[60px] w-[50%] bg-blue-500 flex justify-end items-center gap-3 text-white max-sm:justify-end px-6">
         <Link href={"/"} className="hover:text-orange-300">
